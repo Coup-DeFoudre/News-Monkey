7bdf7372f08da2d8113e9065053e5047b1cf7dfa
Rishabh Pathak <90998740+Coup-DeFoudre@users.noreply.github.com>
2023-07-10 19:15:18 +0530

    Final commit before bug fixing done on 2021-10-20 22:00:00

--- src/App.js
+++ src/App.js
@@ -1,111 +1,93 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import News from "./News";
 
 import Categories from "./components/Categories";
 import LoadingBar from "react-top-loading-bar";
 
-export default class App extends Component {
-  state = {
+const App = (props) => {
+  const [state, setState] = useState({
     progress: 0,
-  };
-  setProgress = (progress) => {
-    this.setState({ progress: progress });
+  });
+  const { progress } = state;
+
+  const setProgress = (progress) => {
+    setState({ progress: progress });
   };
 
-  render() {
-    return (
-      <BrowserRouter>
-        <Navbar />
-        <LoadingBar color="#f11946" progress={this.state.progress} />
-        <Categories />
-        <Routes>
-          <Route index element={<News setProgress={this.setProgress} />} />
-          <Route
-            path="/"
-            element={
-              <News
-                setProgress={this.setProgress}
-                key="general"
-                category="general"
-              />
-            }
-          />
-          <Route
-            path="/general"
-            element={
-              <News
-                setProgress={this.setProgress}
-                key="general"
-                category="general"
-              />
-            }
-          />
-          <Route
-            path="/business"
-            element={
-              <News
-                setProgress={this.setProgress}
-                key="business"
-                category="business"
-              />
-            }
-          />
-          <Route
-            path="/entertainment"
-            element={
-              <News
-                setProgress={this.setProgress}
-                key="entertainment"
-                category="entertainment"
-              />
-            }
-          />
-          <Route
-            path="/health"
-            element={
-              <News
-                setProgress={this.setProgress}
-                key="health"
-                category="health"
-              />
-            }
-          />
-          <Route
-            path="/science"
-            element={
-              <News
-                setProgress={this.setProgress}
-                key="science"
-                category="science"
-              />
-            }
-          />
-          <Route
-            path="/sports"
-            element={
-              <News
-                setProgress={this.setProgress}
-                key="sports"
-                category="sports"
-              />
-            }
-          />
-          <Route
-            path="/technology"
-            element={
-              <News
-                setProgress={this.setProgress}
-                key="technology"
-                category="technology"
-              />
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
-}
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <LoadingBar color="#f11946" progress={progress} />
+      <Categories />
+      <Routes>
+        <Route index element={<News setProgress={setProgress} />} />
+        <Route
+          path="/"
+          element={
+            <News setProgress={setProgress} key="general" category="general" />
+          }
+        />
+        <Route
+          path="/general"
+          element={
+            <News setProgress={setProgress} key="general" category="general" />
+          }
+        />
+        <Route
+          path="/business"
+          element={
+            <News
+              setProgress={setProgress}
+              key="business"
+              category="business"
+            />
+          }
+        />
+        <Route
+          path="/entertainment"
+          element={
+            <News
+              setProgress={setProgress}
+              key="entertainment"
+              category="entertainment"
+            />
+          }
+        />
+        <Route
+          path="/health"
+          element={
+            <News setProgress={setProgress} key="health" category="health" />
+          }
+        />
+        <Route
+          path="/science"
+          element={
+            <News setProgress={setProgress} key="science" category="science" />
+          }
+        />
+        <Route
+          path="/sports"
+          element={
+            <News setProgress={setProgress} key="sports" category="sports" />
+          }
+        />
+        <Route
+          path="/technology"
+          element={
+            <News
+              setProgress={setProgress}
+              key="technology"
+              category="technology"
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
